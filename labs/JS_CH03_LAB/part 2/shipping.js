232eb6d2e2cdc7b168d708a18956308bd9a8277a
Michael-Rhoades <Michael_Rhoades@insideranken.org>
2024-10-15 08:53:36 -0500

    work

--- labs/JS_CH03_LAB/part 2/shipping.js
+++ labs/JS_CH03_LAB/part 2/shipping.js
@@ -1,22 +1,29 @@
 "use strict";
 
 let prePrice = document.querySelector("#prePrice");
-prePrice.value = 0;
-let shipping = 0;
+prePrice.value = parseInt(0);
+let shipping = parseInt(0);
 
 document.getElementById('calc').onclick = () => {
   prePrice = prePrice.value;
-  prePrice = parseFloat(prePrice).toFixed(2);
+  prePrice = parseFloat(prePrice);
 
   if (prePrice <= 25) {
     shipping = parseFloat(1.50);
-    alert("Total cost is $" + parseFloat(prePrice + shipping));
+    prePrice = parseFloat(prePrice + shipping).toFixed(2);
+    alert("Total cost is $" + prePrice);
   }
   else if (prePrice > 25) {
-    shipping = parseFloat(prePrice * 10 / 100).toFixed(2);
-    alert("Total cost is $" + parseFloat(prePrice + shipping));
+    shipping = parseFloat(prePrice * 10 / 100);
+    prePrice = parseFloat(prePrice + shipping).toFixed(2);
+    alert("Total cost is $" + prePrice);
+    prePrice.value = parseInt(0);
   }
   else {
     alert("The entry needs to be a number");
   }
+}
+
+document.getElementById('test').onclick = () => {
+  document.write(`<p>prePrice: ${prePrice}<br>shipping: ${shipping}</p>`)
 }
